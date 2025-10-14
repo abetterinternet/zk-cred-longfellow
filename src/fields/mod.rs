abetterinternet/zk-cred-rs

Split field element serialization into new trait

src/fields/mod.rs
@@ -13,11 +13,14 @@ use std::{
 };
 use subtle::{Choice, ConstantTimeEq};
 
+/// An element of a finite field.
 pub trait FieldElement:
     Debug
     + Clone
     + Copy
     + ConstantTimeEq
+    + PartialEq
+    + Eq
     + Default
     + From<u64>
     + Add<Output = Self>
@@ -30,18 +33,16 @@ pub trait FieldElement:
     + for<'a> Mul<&'a Self, Output = Self>
     + MulAssign
     + Neg<Output = Self>
-    + for<'a> TryFrom<&'a [u8], Error = anyhow::Error>
-    + Codec
 {
-    const NUM_BITS: u32;
+    /// The additive identity of the field.
     const ZERO: Self;
+    /// The multiplicative of the field.
     const ONE: Self;
+    /// 1 + 1 in the field, if applicable.
+    ///
+    /// TODO: This will need to be renamed for characteristic two fields. We need a third evaluation
+    /// point for the sumcheck protocol, and the spec's convention is to use x for GF(2)\[x\]/(Q(x)).
     const TWO: Self;
-
-    /// Number of bytes needed to represent a field element.
-    fn num_bytes() -> usize {
-        (Self::NUM_BITS as usize).div_ceil(8)
-    }
 
     /// Project an integer into the field.
     fn from_u128(value: u128) -> Self;
@@ -49,6 +50,19 @@ pub trait FieldElement:
     /// Test whether this element is zero.
     fn is_zero(&self) -> Choice {
         self.ct_eq(&Self::ZERO)
+    }
+}
+
+/// An element of a finite field with a defined serialization format.
+pub trait WireFieldElement:
+    FieldElement + for<'a> TryFrom<&'a [u8], Error = anyhow::Error> + Codec
+{
+    /// Number of bits needed to represent a field element.
+    const NUM_BITS: u32;
+
+    /// Number of bytes needed to represent a field element.
+    fn num_bytes() -> usize {
+        (Self::NUM_BITS as usize).div_ceil(8)
     }
 
     /// Generate a field element by rejection sampling.
@@ -201,7 +215,7 @@ mod tests {
     use crate::{
         Codec,
         fields::{
-            FieldElement, FieldId, SerializedFieldElement, fieldp128::FieldP128,
+            FieldElement, FieldId, SerializedFieldElement, WireFieldElement, fieldp128::FieldP128,
             fieldp256::FieldP256,
         },
     };
@@ -332,7 +346,7 @@ mod tests {
     }
 
     #[allow(clippy::op_ref, clippy::eq_op)]
-    fn field_element_test<F: FieldElement>() {
+    fn field_element_test_large_characteristic<F: FieldElement>() {
         let three = F::from(3);
         let nine = F::from(9);
         let neg_one = -F::ONE;
@@ -387,7 +401,12 @@ mod tests {
         let mut temp = three;
         temp -= F::ONE;
         assert_eq!(temp, F::TWO);
+    }
 
+    fn field_element_test_codec<F: WireFieldElement>() {
+        let three = F::from(3);
+        let nine = F::from(9);
+        let neg_one = -F::ONE;
         for x in [F::ZERO, F::ONE, three, nine, neg_one] {
             let encoded = x.get_encoded().unwrap();
             assert_eq!(encoded.len(), F::num_bytes());
@@ -412,17 +431,20 @@ mod tests {
 
     #[test]
     fn test_field_p256() {
-        field_element_test::<FieldP256>();
+        field_element_test_large_characteristic::<FieldP256>();
+        field_element_test_codec::<FieldP256>();
     }
 
     #[test]
     fn test_field_p128() {
-        field_element_test::<FieldP128>();
+        field_element_test_large_characteristic::<FieldP128>();
+        field_element_test_codec::<FieldP128>();
     }
 
     #[test]
     fn test_field_p521() {
-        field_element_test::<FieldP521>();
+        field_element_test_large_characteristic::<FieldP521>();
+        field_element_test_codec::<FieldP521>();
     }
 
     #[test]

src/sumcheck/mod.rs
@@ -4,7 +4,7 @@
 
 use crate::{
     circuit::{Circuit, CircuitLayer, Evaluation},
-    fields::FieldElement,
+    fields::WireFieldElement,
     sumcheck::bind::{ElementwiseSum, SumcheckArray},
     transcript::Transcript,
 };
@@ -18,7 +18,7 @@ pub struct Proof<FieldElement> {
     layers: Vec<ProofLayer<FieldElement>>,
 }
 
-impl<FE: FieldElement> Proof<FE> {
+impl<FE: WireFieldElement> Proof<FE> {
     /// Decode a proof from the bytes. This can't be an implementation of [`Codec`] because we need
     /// the circuit this is a proof of to know how many layers there are.
     pub fn decode(
@@ -46,7 +46,7 @@ impl<FE: FieldElement> Proof<FE> {
     }
 }
 
-impl<FE: FieldElement> Proof<FE> {
+impl<FE: WireFieldElement> Proof<FE> {
     /// Construct a padded proof of the transcript of the given evaluation of the circuit and return
     /// the prover messages needed for the verifier to reconstruct the transcript.
     pub fn new<PadGenerator>(
@@ -307,7 +307,7 @@ struct ProofLayer<FieldElement> {
 /// Proof layer serialization corresponds to PaddedTranscriptLayer in [7.3][1].
 ///
 /// https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-01#section-7.3
-impl<FE: FieldElement> ProofLayer<FE> {
+impl<FE: WireFieldElement> ProofLayer<FE> {
     /// Decode a proof layer from the bytes. We can't implement [`Codec`] here because we need some
     /// context (the corresponding circuit layer) to determine how many elements the layer should
     /// contain.
@@ -391,7 +391,11 @@ struct Polynomial<FE> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{Size, circuit::tests::CircuitTestVector, fields::fieldp128::FieldP128};
+    use crate::{
+        Size,
+        circuit::tests::CircuitTestVector,
+        fields::{FieldElement, fieldp128::FieldP128},
+    };
     use std::io::Cursor;
 
     #[test]

src/transcript.rs
@@ -3,7 +3,7 @@
 //!
 //! https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-00#section-3
 
-use crate::fields::FieldElement;
+use crate::fields::WireFieldElement;
 use aes::{
     Aes256,
     cipher::{BlockEncrypt, KeyInit},
@@ -71,7 +71,7 @@ impl Transcript {
     /// Write a field element to the transcript.
     ///
     /// https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-00#section-3.1.2
-    pub fn write_field_element<FE: FieldElement>(
+    pub fn write_field_element<FE: WireFieldElement>(
         &mut self,
         field_element: &FE,
     ) -> Result<(), anyhow::Error> {
@@ -84,7 +84,7 @@ impl Transcript {
         Ok(())
     }
 
-    pub fn write_field_element_array<FE: FieldElement>(
+    pub fn write_field_element_array<FE: WireFieldElement>(
         &mut self,
         field_elements: &[FE],
     ) -> Result<(), anyhow::Error> {
@@ -156,7 +156,7 @@ impl Transcript {
     }
 
     /// Generate a challenge, consisting of `length` field elements.
-    pub fn generate_challenge<FE: FieldElement>(
+    pub fn generate_challenge<FE: WireFieldElement>(
         &mut self,
         length: usize,
     ) -> Result<Vec<FE>, anyhow::Error> {
@@ -209,7 +209,7 @@ impl FiatShamirPseudoRandomFunction {
     }
 
     /// Sample a field element from this FSPRF.
-    pub fn sample_field_element<FE: FieldElement>(&mut self) -> FE {
+    pub fn sample_field_element<FE: WireFieldElement>(&mut self) -> FE {
         FE::sample_from_source(|num_bytes| self.take(num_bytes).collect::<Vec<_>>())
     }
 }

abetterinternet/zk-cred-longfellow

src/fields/mod.rs
@@ -13,11 +13,14 @@ use std::{
 };
 use subtle::{Choice, ConstantTimeEq};
 
+/// An element of a finite field.
 pub trait FieldElement:
     Debug
     + Clone
     + Copy
     + ConstantTimeEq
+    + PartialEq
+    + Eq
     + Default
     + From<u64>
     + Add<Output = Self>
@@ -30,18 +33,16 @@ pub trait FieldElement:
     + for<'a> Mul<&'a Self, Output = Self>
     + MulAssign
     + Neg<Output = Self>
-    + for<'a> TryFrom<&'a [u8], Error = anyhow::Error>
-    + Codec
 {
-    const NUM_BITS: u32;
+    /// The additive identity of the field.
     const ZERO: Self;
+    /// The multiplicative of the field.
     const ONE: Self;
+    /// 1 + 1 in the field, if applicable.
+    ///
+    /// TODO: This will need to be renamed for characteristic two fields. We need a third evaluation
+    /// point for the sumcheck protocol, and the spec's convention is to use x for GF(2)\[x\]/(Q(x)).
     const TWO: Self;
-
-    /// Number of bytes needed to represent a field element.
-    fn num_bytes() -> usize {
-        (Self::NUM_BITS as usize).div_ceil(8)
-    }
 
     /// Project an integer into the field.
     fn from_u128(value: u128) -> Self;
@@ -49,6 +50,19 @@ pub trait FieldElement:
     /// Test whether this element is zero.
     fn is_zero(&self) -> Choice {
         self.ct_eq(&Self::ZERO)
+    }
+}
+
+/// An element of a finite field with a defined serialization format.
+pub trait CodecFieldElement:
+    FieldElement + for<'a> TryFrom<&'a [u8], Error = anyhow::Error> + Codec
+{
+    /// Number of bits needed to represent a field element.
+    const NUM_BITS: u32;
+
+    /// Number of bytes needed to represent a field element.
+    fn num_bytes() -> usize {
+        (Self::NUM_BITS as usize).div_ceil(8)
     }
 
     /// Generate a field element by rejection sampling.
@@ -201,7 +215,7 @@ mod tests {
     use crate::{
         Codec,
         fields::{
-            FieldElement, FieldId, SerializedFieldElement, fieldp128::FieldP128,
+            CodecFieldElement, FieldElement, FieldId, SerializedFieldElement, fieldp128::FieldP128,
             fieldp256::FieldP256,
         },
     };
@@ -332,7 +346,7 @@ mod tests {
     }
 
     #[allow(clippy::op_ref, clippy::eq_op)]
-    fn field_element_test<F: FieldElement>() {
+    fn field_element_test_large_characteristic<F: FieldElement>() {
         let three = F::from(3);
         let nine = F::from(9);
         let neg_one = -F::ONE;
@@ -387,7 +401,12 @@ mod tests {
         let mut temp = three;
         temp -= F::ONE;
         assert_eq!(temp, F::TWO);
+    }
 
+    fn field_element_test_codec<F: CodecFieldElement>() {
+        let three = F::from(3);
+        let nine = F::from(9);
+        let neg_one = -F::ONE;
         for x in [F::ZERO, F::ONE, three, nine, neg_one] {
             let encoded = x.get_encoded().unwrap();
             assert_eq!(encoded.len(), F::num_bytes());
@@ -412,17 +431,20 @@ mod tests {
 
     #[test]
     fn test_field_p256() {
-        field_element_test::<FieldP256>();
+        field_element_test_large_characteristic::<FieldP256>();
+        field_element_test_codec::<FieldP256>();
     }
 
     #[test]
     fn test_field_p128() {
-        field_element_test::<FieldP128>();
+        field_element_test_large_characteristic::<FieldP128>();
+        field_element_test_codec::<FieldP128>();
     }
 
     #[test]
     fn test_field_p521() {
-        field_element_test::<FieldP521>();
+        field_element_test_large_characteristic::<FieldP521>();
+        field_element_test_codec::<FieldP521>();
     }
 
     #[test]

src/sumcheck/mod.rs
@@ -4,7 +4,7 @@
 
 use crate::{
     circuit::{Circuit, CircuitLayer, Evaluation},
-    fields::FieldElement,
+    fields::CodecFieldElement,
     sumcheck::bind::{ElementwiseSum, SumcheckArray},
     transcript::Transcript,
 };
@@ -18,7 +18,7 @@ pub struct Proof<FieldElement> {
     layers: Vec<ProofLayer<FieldElement>>,
 }
 
-impl<FE: FieldElement> Proof<FE> {
+impl<FE: CodecFieldElement> Proof<FE> {
     /// Decode a proof from the bytes. This can't be an implementation of [`Codec`] because we need
     /// the circuit this is a proof of to know how many layers there are.
     pub fn decode(
@@ -46,7 +46,7 @@ impl<FE: FieldElement> Proof<FE> {
     }
 }
 
-impl<FE: FieldElement> Proof<FE> {
+impl<FE: CodecFieldElement> Proof<FE> {
     /// Construct a padded proof of the transcript of the given evaluation of the circuit and return
     /// the prover messages needed for the verifier to reconstruct the transcript.
     pub fn new<PadGenerator>(
@@ -314,7 +314,7 @@ struct ProofLayer<FieldElement> {
 /// Proof layer serialization corresponds to PaddedTranscriptLayer in [7.3][1].
 ///
 /// https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-01#section-7.3
-impl<FE: FieldElement> ProofLayer<FE> {
+impl<FE: CodecFieldElement> ProofLayer<FE> {
     /// Decode a proof layer from the bytes. We can't implement [`Codec`] here because we need some
     /// context (the corresponding circuit layer) to determine how many elements the layer should
     /// contain.
@@ -398,7 +398,11 @@ struct Polynomial<FE> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{Size, circuit::tests::CircuitTestVector, fields::fieldp128::FieldP128};
+    use crate::{
+        Size,
+        circuit::tests::CircuitTestVector,
+        fields::{FieldElement, fieldp128::FieldP128},
+    };
     use std::io::Cursor;
 
     #[test]

src/transcript.rs
@@ -3,7 +3,7 @@
 //!
 //! https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-00#section-3
 
-use crate::fields::FieldElement;
+use crate::fields::CodecFieldElement;
 use aes::{
     Aes256,
     cipher::{BlockEncrypt, KeyInit},
@@ -71,7 +71,7 @@ impl Transcript {
     /// Write a field element to the transcript.
     ///
     /// https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-00#section-3.1.2
-    pub fn write_field_element<FE: FieldElement>(
+    pub fn write_field_element<FE: CodecFieldElement>(
         &mut self,
         field_element: &FE,
     ) -> Result<(), anyhow::Error> {
@@ -84,7 +84,7 @@ impl Transcript {
         Ok(())
     }
 
-    pub fn write_field_element_array<FE: FieldElement>(
+    pub fn write_field_element_array<FE: CodecFieldElement>(
         &mut self,
         field_elements: &[FE],
     ) -> Result<(), anyhow::Error> {
@@ -155,7 +155,7 @@ impl Transcript {
     }
 
     /// Generate a challenge, consisting of `length` field elements.
-    pub fn generate_challenge<FE: FieldElement>(
+    pub fn generate_challenge<FE: CodecFieldElement>(
         &mut self,
         length: usize,
     ) -> Result<Vec<FE>, anyhow::Error> {
@@ -208,7 +208,7 @@ impl FiatShamirPseudoRandomFunction {
     }
 
     /// Sample a field element from this FSPRF.
-    pub fn sample_field_element<FE: FieldElement>(&mut self) -> FE {
+    pub fn sample_field_element<FE: CodecFieldElement>(&mut self) -> FE {
         FE::sample_from_source(|num_bytes| self.take(num_bytes).collect::<Vec<_>>())
     }
 }
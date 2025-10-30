34f2fd9c617013c49e8f0a382e426fc39c54f788
Tim Geoghegan <timg@divviup.org>
2025-10-30 13:09:52 -0400

    skeleton of Ligero module

---
+++ src/ligero/mod.rs
@@ -0,0 +1,7 @@
+//! Ligero proof system, per [Section 4][1].
+//!
+//! [1]: https://datatracker.ietf.org/doc/html/draft-google-cfrg-libzk-01#section-4
+
+pub mod committer;
+pub mod prover;
+pub mod verifier;

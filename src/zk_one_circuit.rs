21dc1f579c002b688b499ac91407fb0ccaf0a433
David Cook <dcook@divviup.org>
2025-11-26 12:23:31 -0600

    Add mutation testing

--- src/zk_one_circuit.rs
+++ src/zk_one_circuit.rs
@@ -9,8 +9,12 @@ mod tests {
         decode_test_vector,
         fields::fieldp128::FieldP128,
         test_vector::CircuitTestVector,
-        zk_one_circuit::{prover::Prover, verifier::Verifier},
+        zk_one_circuit::{
+            prover::{Proof, Prover},
+            verifier::Verifier,
+        },
     };
+    use std::io::Cursor;
     use wasm_bindgen_test::wasm_bindgen_test;
 
     #[wasm_bindgen_test(unsupported = test)]
@@ -30,5 +34,47 @@ mod tests {
 
         let verifier = Verifier::new(&circuit, ligero_parameters);
         verifier.verify(public_inputs, &proof).unwrap();
+    }
+
+    #[ignore = "slow test"]
+    #[wasm_bindgen_test(unsupported = test)]
+    fn longfellow_rfc_1_87474f308020535e57a778a82394a14106f8be5b_mutation() {
+        let (test_vector, circuit) =
+            decode_test_vector!("longfellow-rfc-1-87474f308020535e57a778a82394a14106f8be5b");
+        let ligero_parameters = test_vector.ligero_parameters();
+        let all_inputs: Vec<FieldP128> = test_vector.valid_inputs();
+        let public_inputs = &all_inputs[..circuit.num_public_inputs() - 1];
+        let session_id = b"testtesttesttesttesttesttesttest";
+
+        let prover = Prover::new(&circuit, ligero_parameters.clone());
+        let proof = prover.prove(session_id, &all_inputs).unwrap();
+
+        let mut encoded = Vec::new();
+        proof.encode(&mut encoded).unwrap();
+
+        let verifier = Verifier::new(&circuit, ligero_parameters);
+
+        // Mutation testing: flip each bit in a proof, and confirm that it either fails to
+        // deserialize or fails to verify.
+        let mut decode_failure_count = 0;
+        let mut verify_failure_count = 0;
+        for byte_offset in 0..encoded.len() {
+            println!("{byte_offset}/{}", encoded.len());
+            for bit_offset in 0..8 {
+                let mut modified = encoded.clone();
+                modified[byte_offset] ^= 1 << bit_offset;
+
+                let Ok(decoded) =
+                    Proof::<FieldP128>::decode(&verifier, &mut Cursor::new(&modified))
+                else {
+                    decode_failure_count += 1;
+                    continue;
+                };
+                verifier.verify(public_inputs, &decoded).unwrap_err();
+                verify_failure_count += 1;
+            }
+        }
+        println!("decoding failed {decode_failure_count} times");
+        println!("verifying failed {verify_failure_count} times");
     }
 }

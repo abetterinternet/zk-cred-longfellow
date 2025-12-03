6640ee1abf2475639b73e060a93cab75cb04cbcf
David Cook <divergentdave@gmail.com>
2025-12-03 10:49:39 -0600

    Add initial benchmarks (#78)
    * Add initial benchmarks
    
    * Disable rayon

---
+++ benches/extend.rs
@@ -0,0 +1,69 @@
+use criterion::{
+    BenchmarkGroup, BenchmarkId, Criterion, criterion_group, criterion_main, measurement::WallTime,
+};
+use std::{hint::black_box, time::Duration};
+use zk_cred_longfellow::fields::{
+    LagrangePolynomialFieldElement, fieldp128::FieldP128, fieldp256::FieldP256,
+    fieldp521::FieldP521,
+};
+
+fn benchmark_extend<FE: LagrangePolynomialFieldElement>(g: &mut BenchmarkGroup<WallTime>) {
+    struct Parameters {
+        // Parameters for extend().
+        input_size: usize,
+        output_size: usize,
+
+        // Parameters for configuring Criterion.rs.
+        sample_size: usize,
+        measurement_time: Duration,
+    }
+
+    for Parameters {
+        input_size,
+        output_size,
+        sample_size,
+        measurement_time,
+    } in [
+        Parameters {
+            input_size: 8,
+            output_size: 16,
+            sample_size: 100,
+            measurement_time: Duration::from_secs(5),
+        },
+        Parameters {
+            input_size: 100,
+            output_size: 200,
+            sample_size: 10,
+            measurement_time: Duration::from_secs(30),
+        },
+    ] {
+        g.sample_size(sample_size);
+        g.measurement_time(measurement_time);
+        let input = vec![FE::ZERO; input_size];
+        g.bench_function(
+            BenchmarkId::from_parameter(format_args!("{input_size}_to_{output_size}")),
+            |b| {
+                b.iter(|| FE::extend(black_box(&input), black_box(output_size)));
+            },
+        );
+    }
+}
+
+fn benchmark_all(c: &mut Criterion) {
+    let mut g = c.benchmark_group("extend_fieldp128");
+    benchmark_extend::<FieldP128>(&mut g);
+    g.finish();
+
+    let mut g = c.benchmark_group("extend_fieldp256");
+    benchmark_extend::<FieldP256>(&mut g);
+    g.finish();
+
+    let mut g = c.benchmark_group("extend_fieldp521");
+    benchmark_extend::<FieldP521>(&mut g);
+    g.finish();
+
+    // TODO(#56): Add benchmark for GF(2^128) as well.
+}
+
+criterion_group!(benches, benchmark_all);
+criterion_main!(benches);
---
+++ benches/longfellow_one_field.rs
@@ -0,0 +1,61 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::{fs, hint::black_box, io::Cursor};
+use zk_cred_longfellow::{
+    Codec,
+    circuit::Circuit,
+    fields::fieldp128::FieldP128,
+    ligero::LigeroParameters,
+    zk_one_circuit::{prover::Prover, verifier::Verifier},
+};
+
+fn load_circuit(name: &str) -> Circuit {
+    let compressed = fs::read(format!("test-vectors/circuit/{name}.circuit.zst")).unwrap();
+    let bytes = zstd::decode_all(compressed.as_slice()).unwrap();
+    Circuit::decode(&mut Cursor::new(&bytes)).unwrap()
+}
+
+fn rfc_1(c: &mut Criterion) {
+    let circuit = load_circuit("longfellow-rfc-1-87474f308020535e57a778a82394a14106f8be5b");
+    let ligero_parameters = LigeroParameters {
+        nreq: 6,
+        witnesses_per_row: 15,
+        quadratic_constraints_per_row: 2,
+        block_size: 21,
+        num_columns: 128,
+    };
+
+    let session_id = b"test";
+    let inputs = &[FieldP128::from(45), FieldP128::from(5), FieldP128::from(6)];
+
+    let prover = Prover::new(&circuit, ligero_parameters.clone());
+
+    let mut g = c.benchmark_group("rfc_1");
+
+    g.sample_size(50);
+    g.bench_function("prove", |b| {
+        b.iter(|| {
+            prover
+                .prove(black_box(session_id), black_box(inputs))
+                .unwrap()
+        });
+    });
+
+    let proof = prover.prove(session_id, inputs).unwrap();
+    let public_inputs = &inputs[..1];
+
+    let verifier = Verifier::new(&circuit, ligero_parameters);
+
+    g.sample_size(50);
+    g.bench_function("verify", |b| {
+        b.iter(|| {
+            verifier
+                .verify(black_box(public_inputs), black_box(&proof))
+                .unwrap()
+        })
+    });
+
+    g.finish();
+}
+
+criterion_group!(benches, rfc_1);
+criterion_main!(benches);

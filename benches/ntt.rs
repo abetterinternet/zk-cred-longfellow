831bf670344a1e8ee4a11a82108288fedc743c1e
David Cook <dcook@divviup.org>
2025-12-11 15:40:00 -0600

    Add inverse NTT benchmark

--- benches/ntt.rs
+++ benches/ntt.rs
@@ -11,6 +11,25 @@ fn benchmark_ntt<FE: NttFieldElement>(g: &mut BenchmarkGroup<WallTime>) {
             let omegas = FE::omegas();
             b.iter(|| FE::ntt_bit_reversed(black_box(&mut values), black_box(&omegas)));
         });
+        g.bench_function(BenchmarkId::new("inverse_ntt", size), |b| {
+            let mut values = vec![FE::ONE; size];
+            let omega_inverses = FE::omega_inverses();
+            let mut size_inverse = FE::ONE;
+            for _ in 0..size.ilog2() {
+                size_inverse *= FE::HALF;
+            }
+            assert_eq!(
+                size_inverse * FE::from_u128(size.try_into().unwrap()),
+                FE::ONE
+            );
+            b.iter(|| {
+                FE::inverse_ntt_bit_reversed(
+                    black_box(&mut values),
+                    black_box(&omega_inverses),
+                    black_box(size_inverse),
+                )
+            });
+        });
     }
 }
 

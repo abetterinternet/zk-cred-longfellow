1423d27edab34b18b03d44a5c0df0cbb091c6437
Tim Geoghegan <timg@divviup.org>
2025-12-11 12:50:51 -0800

    jcjones review

--- src/fields/field2_128/extend.rs
+++ src/fields/field2_128/extend.rs
@@ -21,13 +21,12 @@ pub struct ExtendContext {
 /// [1]: https://eprint.iacr.org/2024/2010.pdf
 fn twiddle(power: u32, mut coset: usize) -> Field2_128 {
     let mut accumulator = Field2_128::ZERO;
-    let mut position = 0;
-    while coset > 0 {
+    for position in 0..usize::BITS {
+        // TODO: make this constant time by removing the branch on `coset`
         if coset & 1 == 1 {
-            accumulator += twiddle_array_at(power, position);
+            accumulator += twiddle_array_at(power, position)
         }
         coset >>= 1;
-        position += 1;
     }
 
     accumulator

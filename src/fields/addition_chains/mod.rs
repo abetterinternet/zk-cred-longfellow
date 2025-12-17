5a1e7870c3afee766fdf1ad63d9f02ce006fc2ab
David Cook <divergentdave@gmail.com>
2025-12-17 12:53:35 -0600

    Add P-256 scalar field implementation (#96)
    * Add P-256 scalar field implementation
    
    * Prune unused fiat-crypto routines

--- src/fields/addition_chains/mod.rs
+++ src/fields/addition_chains/mod.rs
@@ -1,3 +1,4 @@
 pub(super) mod gf_2_128_m2;
 pub(super) mod p128m2;
+pub(super) mod p256_scalar_m2;
 pub(super) mod p256m2;
--- src/fields/codegen_addchain.sh
+++ src/fields/codegen_addchain.sh
@@ -11,4 +11,5 @@ cd "$(dirname "$0")"/addition_chains
 
 addchain gen -tmpl template.txt -out p128m2.rs p128m2.acc
 addchain gen -tmpl template.txt -out p256m2.rs p256m2.acc
+addchain gen -tmpl template.txt -out p256_scalar_m2.rs p256_scalar_m2.acc
 addchain gen -tmpl template.txt -out gf_2_128_m2.rs gf_2_128_m2.acc
